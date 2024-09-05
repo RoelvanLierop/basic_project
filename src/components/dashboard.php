0ef28c2f8e536d4cc54801ecde3f31038cbf65b6
Roel van Lierop <roel.van.lierop@gmail.com>
2024-09-05 13:20:26 +0200

    Added some text on the home page

--- src/components/dashboard.php
+++ src/components/dashboard.php
@@ -25,7 +25,43 @@
     <div class="row mt-3">
         <div class="offset-1 col-7">
             <div class="border-primary border rounded p-3">
-                <h3>Welcome</h3>
+                <h2>Welcome</h2>
+                <p>
+                    This tool was created to show some basic functionality, by using plain PHP, basic OOP, and AJAX.
+                    Designed as a single page application, we dive into something I never did before.
+                    To support this, we use Javascript and Ajax to load pages and process data.
+                <p>
+                <p>
+                    This is not a MVC project, because I wanted to show that plain PHP is still a skill I possess, and that I can use accordingly.
+                    When using MVC, I usually prefer a framework like Vue, Laravel, or Yii2. These provide the basis for more elegant coding.
+                    In this case the premise was to show more "vanilla" skills instead of relying om a framework to do most of the work.
+                    It meant writing plain PHP, MySQL, HTML, and CSS to show these skills, ranging from inline php, to objects and helper methods.
+                </p>
+                <p>
+                    All in all, I still prefer a framework to do the heavy lifting, and the codebase could be smaller and faster, but there should always be room to improve and update.
+                </p>
+                <h3>Fun Stuff</h3>
+                <p>Database</p>
+                <ul>
+                    <li>The database is created with plain MySQL Statements.</li>
+                    <li>The users table has a composite unique index, which includes display_name and email.</li>
+                    <li>The users table has a composite index, which includes display_name and email for searching.</li>
+                    <li>The messages table has a search index for the message contents.</li>
+                <p>Installer</p>
+                <ul>
+                    <li>I added a Installer file, which you already encountered (that's why you're reading this).</li>
+                </ul>
+                <p>General</p>
+                <ul>
+                    <li>On contrary of the assignment, I tried making the project a single page application.</li>
+                    <li>All pages are components, loaded with AJAX.</li>
+                    <li>All forms have client-side validation, next to back-end validation of data.</li>
+                </ul>
+                <p>Login / Register / Search</p>
+                <ul>
+                    <li>Form submissions are sent as AJAX calls, which load the dashboard when succesful.</li>
+                    <li>Searching ranges across multiple fields, the groups are id, email/display_name, and author/message.</li>
+                </ul>
             </div>
         </div>
         <div class="col-3">
