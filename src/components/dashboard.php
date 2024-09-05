1c05f6a01c6db61698ea8a6d77801b66e33ff1f0
Roel van Lierop <roel.van.lierop@gmail.com>
2024-09-05 13:33:24 +0200

    Updated Readme.md

--- src/components/dashboard.php
+++ src/components/dashboard.php
@@ -41,23 +41,20 @@
                     All in all, I still prefer a framework to do the heavy lifting, and the codebase could be smaller and faster, but there should always be room to improve and update.
                 </p>
                 <h3>Fun Stuff</h3>
-                <p>Database</p>
+                <p class="mt-3">Database</p>
                 <ul>
                     <li>The database is created with plain MySQL Statements.</li>
                     <li>The users table has a composite unique index, which includes display_name and email.</li>
                     <li>The users table has a composite index, which includes display_name and email for searching.</li>
                     <li>The messages table has a search index for the message contents.</li>
-                <p>Installer</p>
-                <ul>
-                    <li>I added a Installer file, which you already encountered (that's why you're reading this).</li>
                 </ul>
-                <p>General</p>
+                <p class="mt-3">General</p>
                 <ul>
                     <li>On contrary of the assignment, I tried making the project a single page application.</li>
                     <li>All pages are components, loaded with AJAX.</li>
                     <li>All forms have client-side validation, next to back-end validation of data.</li>
                 </ul>
-                <p>Login / Register / Search</p>
+                <p class="mt-3">Login / Register / Search</p>
                 <ul>
                     <li>Form submissions are sent as AJAX calls, which load the dashboard when succesful.</li>
                     <li>Searching ranges across multiple fields, the groups are id, email/display_name, and author/message.</li>
